695bf199b2e414bc36d369948a7a6a470f93580b
Aoyu Zou <zy22021@outlook.com>
2024-11-06 10:11:51 -0800

    updated plotting codes based on review

--- code/graphical.R
+++ code/graphical.R
@@ -2,7 +2,7 @@
 require(pacman)
 
 # load packages using pacman
-pacman::p_load(tidyverse, lubridate, here, stats, zoo, scales, ggpubr, patchwork, RColorBrewer)
+pacman::p_load(tidyverse, lubridate, here, stats, zoo, scales, lvplot, ggpubr, gridExtra, patchwork, RColorBrewer)
 
 # turn off scientific notation
 options(scipen = 999, digits = 15)
@@ -31,9 +31,11 @@ ls_colors <- c("Baseline" = "#c6dbef",
                "Projected baseline" = "#2171b5",
                "Intervention" = "#fdbb84",
                "Measured interv" = "#fdbb84",
-               "Conventional M&V" = "grey70", 
-               "Randomized M&V" = "#66c2a4", 
-               "Buildings satisfying all criteria" = "#66c2a4", 
+               "Conventional" = "grey70", 
+               "Randomized" = "#66c2a4", 
+               "Randomized\n(24 months)" = "#238b45",
+               "Buildings (tidy) finishing randomized M&V" = "#bcbddc",
+               "Buildings (messy) finishing randomized M&V" = "#756bb1",
                "Buildings satisfying 80% TMY range" = "black", 
                "Buildings satisfying SPRT" = "black")
 
@@ -145,7 +147,7 @@ df_loc <- read.csv("../readfiles/loc_map.csv")
 
 # Tidy dataset
 readfile_tidy <- str_glue("../readfiles/tidy/")
-fig_path = "../figs/manuscript/"
+fig_path = "../figs/graphical_abs/"
 
 df_energy_tidy <- read_rds(paste0(readfile_tidy, "df_energy.rds"))
 df_meta_tidy <- read_rds(paste0(readfile_tidy, "df_meta.rds"))
@@ -220,107 +222,244 @@ source(paste0(function_path, "prepost_plot.R"))
 rand_eob_T <- df_seq_FS_tidy %>% 
   filter(seq == "eob") %>% 
   left_join(df_FS_tidy %>% filter(scenario == "ref" & method == "true"), by = c("name", "site")) %>% 
-  mutate(rand_diff_eob = savings - FS) %>% 
-  select(name, site, rand_diff_eob)
+  mutate(diff = savings - FS, 
+         method = "rand_eob") %>% 
+  select(name, method, diff)
 
 conv_T <- df_FS_tidy %>% 
   filter(method != "rand") %>% 
   pivot_wider(names_from = method, values_from = savings) %>% 
-  mutate(conv_diff = true - conv) %>% 
-  select(name, site, conv_diff)
+  mutate(diff = true - conv, 
+         method = "conv") %>% 
+  select(name, method, diff)
 
-rand_final_T <- df_seq_FS_tidy %>% 
-  filter(seq == "final") %>% 
-  left_join(df_FS_tidy %>% filter(scenario == "ref" & method == "true"), by = c("name", "site")) %>% 
-  mutate(rand_diff_final = savings - FS) %>% 
-  select(name, site, rand_diff_final)
+rand_final_T <- df_FS_tidy %>% 
+  filter(method != "conv") %>% 
+  pivot_wider(names_from = method, values_from = savings) %>% 
+  mutate(diff = true - rand, 
+         method = "rand_final") %>% 
+  select(name, method, diff)
 
-p1 <- ggplot() +
-  geom_boxplot(data = conv_T, aes(x = "Conventional", y = conv_diff, fill = "Conventional M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_eob_T, aes(x = "Randomized", y = rand_diff_eob, fill = "Randomized M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_final_T, aes(x = "Randomized\n(24 months)", y = rand_diff_final, fill = "Randomized M&V (24 months)"), outlier.shape = NA) +
+df_acc_T <- rbind(rand_eob_T, conv_T, rand_final_T)
+
+p1 <- df_acc_T %>% 
+  mutate(method = as.factor(method), 
+         method = recode_factor(method, "conv" = "Conventional", "rand_eob" = "Randomized", "rand_final" = "Randomized\n(24 months)")) %>% 
+  ggplot(aes(x = method, y = diff, fill = method)) +
+  geom_lv(alpha = 0.4, k = 4, outlier.size = 0.5) +
+  geom_boxplot(outlier.alpha = 0, coef = 0, fill = "#00000000", aes(color = method)) +
+  geom_hline(yintercept = 0, color = "#fb8072", linewidth = 1, lty = "dashed") +
+  geom_text(data = . %>% group_by(method) %>% summarise(mean = mean(diff)) %>% ungroup(), 
+            aes(x = method, y = mean, label = paste0(round(mean, digits = 1), " %"))) +
   scale_y_continuous(expand = c(0, 0), 
-                     breaks = breaks_pretty(n = 3), 
+                     breaks = breaks_pretty(n = 4), 
                      labels = number_format(suffix = " %")) +
   scale_fill_manual(values = ls_colors) +
+  scale_color_manual(values = c("grey80", "grey80", "grey80")) + 
   labs(fill = NULL, 
        x = NULL, 
        y = NULL, 
-       subtitle = "Tidy set") +
-  coord_cartesian(ylim = c(-8, 8)) +
+       subtitle = str_glue("Tidy set ({nrow(df_acc_T)/3} buildings)")) +
+  coord_cartesian(ylim = c(-18, 18)) +
   theme(panel.grid.major.y = element_line(color = "grey80", linewidth = 0.25),
-        legend.direction = "horizontal",
-        legend.position = "bottom",
+        legend.position = "none",
         axis.text.y = element_blank(), 
         plot.margin = margin(t = 2, r = 7, b = 2, l = 2, unit = "mm"))
 
 # plot for the messy subset
 rand_eob_M <- df_seq_FS_messy %>% 
   filter(seq == "eob") %>% 
-  left_join(df_FS_messy %>% filter(scenario == "ref" & method == "true"), by = c("name", "site")) %>% 
-  mutate(rand_diff_eob = savings - FS) %>% 
-  select(name, site, rand_diff_eob)
+  left_join(df_FS_messy %>% filter(scenario == "ref" & method == "true"), by = c("name", "site"))%>% 
+  mutate(diff = savings - FS, 
+         method = "rand_eob") %>% 
+  select(name, method, diff)
 
 conv_M <- df_FS_messy %>% 
   filter(method != "rand") %>% 
   pivot_wider(names_from = method, values_from = savings) %>% 
-  mutate(conv_diff = true - conv) %>% 
-  select(name, site, conv_diff)
+  mutate(diff = true - conv, 
+         method = "conv") %>% 
+  select(name, method, diff)
 
-rand_final_M <- df_seq_FS_messy %>% 
-  filter(seq == "final") %>% 
-  left_join(df_FS_messy %>% filter(scenario == "ref" & method == "true"), by = c("name", "site")) %>% 
-  mutate(rand_diff_final = savings - FS) %>% 
-  select(name, site, rand_diff_final)
+rand_final_M <- df_FS_messy %>% 
+  filter(method != "conv") %>% 
+  pivot_wider(names_from = method, values_from = savings) %>% 
+  mutate(diff = true - rand, 
+         method = "rand_final") %>% 
+  select(name, method, diff)
 
-p2 <- ggplot() +
-  geom_boxplot(data = conv_M, aes(x = "Conventional", y = conv_diff, fill = "Conventional M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_eob_M, aes(x = "Randomized", y = rand_diff_eob, fill = "Randomized M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_final_M, aes(x = "Randomized\n(24 months)", y = rand_diff_final, fill = "Randomized M&V (24 months)"), outlier.shape = NA) +
+df_acc_M <- rbind(rand_eob_M, conv_M, rand_final_M)
+
+p2 <- df_acc_M %>% 
+  mutate(method = as.factor(method), 
+         method = recode_factor(method, "conv" = "Conventional", "rand_eob" = "Randomized", "rand_final" = "Randomized\n(24 months)")) %>% 
+  ggplot(aes(x = method, y = diff, fill = method)) +
+  geom_lv(alpha = 0.4, k = 4, outlier.size = 0.5) +
+  geom_boxplot(outlier.alpha = 0, coef = 0, fill = "#00000000", aes(color = method)) +
+  geom_hline(yintercept = 0, color = "#fb8072", linewidth = 1, lty = "dashed") +
+  geom_text(data = . %>% group_by(method) %>% summarise(mean = mean(diff)) %>% ungroup(), 
+            aes(x = method, y = mean, label = paste0(round(mean, digits = 1), " %"))) +
   scale_y_continuous(expand = c(0, 0), 
-                     breaks = breaks_pretty(n = 3), 
+                     breaks = breaks_pretty(n = 4), 
                      labels = number_format(suffix = " %")) +
   scale_fill_manual(values = ls_colors) +
+  scale_color_manual(values = c("grey80", "grey80", "grey80")) + 
   labs(fill = NULL, 
        x = NULL, 
        y = NULL, 
-       subtitle = "Messy set") +
-  coord_cartesian(ylim = c(-8, 8)) +
+       subtitle = str_glue("Messy set ({nrow(df_acc_M)/3} buildings)")) +
+  coord_cartesian(ylim = c(-18, 18)) +
   theme(panel.grid.major.y = element_line(color = "grey80", linewidth = 0.25),
-        legend.direction = "horizontal",
-        legend.position = "bottom",
+        legend.position = "none",
         axis.text.y = element_blank(), 
         plot.margin = margin(t = 2, r = 7, b = 2, l = 2, unit = "mm"))
 
 
 # plot for combined dataset
-rand_eob_A <- bind_rows(rand_eob_M, rand_eob_T)
-conv_A <- bind_rows(conv_M, conv_T)
-rand_final_A <- bind_rows(rand_final_M, rand_final_T)
+rand_eob_A <- bind_rows(rand_eob_M, rand_eob_T) %>% 
+  select(name, rand_eob_diff = diff)
 
-p3 <- ggplot() +
-  geom_boxplot(data = conv_A, aes(x = "Conventional", y = conv_diff, fill = "Conventional M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_eob_A, aes(x = "Randomized", y = rand_diff_eob, fill = "Randomized M&V"), outlier.shape = NA) +
-  geom_boxplot(data = rand_final_A, aes(x = "Randomized\n(24 months)", y = rand_diff_final, fill = "Randomized M&V (24 months)"), outlier.shape = NA) +
+conv_A <- bind_rows(conv_M, conv_T) %>% 
+  select(name, conv_diff = diff)
+
+rand_final_A <- bind_rows(rand_final_M, rand_final_T) %>% 
+  select(name, rand_final_diff = diff)
+
+df_acc_A <- rand_eob_A %>% 
+  left_join(conv_A, by = "name") %>% 
+  left_join(rand_final_A, by = "name") %>% 
+  pivot_longer(c(rand_eob_diff, conv_diff, rand_final_diff), names_to = "method", values_to = "diff")
+
+p3 <- df_acc_A %>% 
+  mutate(method = as.factor(method), 
+         method = recode_factor(method, "conv_diff" = "Conventional", "rand_eob_diff" = "Randomized", "rand_final_diff" = "Randomized\n(24 months)")) %>% 
+  ggplot(aes(x = method, y = diff, fill = method)) +
+  geom_lv(alpha = 0.4, k = 4, outlier.size = 0.5) +
+  geom_boxplot(outlier.alpha = 0, coef = 0, fill = "#00000000", aes(color = method)) +
+  geom_hline(yintercept = 0, color = "#fb8072", linewidth = 1, lty = "dashed") +
+  geom_text(data = . %>% group_by(method) %>% summarise(mean = mean(diff)) %>% ungroup(), 
+            aes(x = method, y = mean, label = paste0(round(mean, digits = 1), " %"))) +
   scale_y_continuous(expand = c(0, 0), 
-                     breaks = breaks_pretty(n = 3), 
+                     breaks = breaks_pretty(n = 4), 
                      labels = number_format(suffix = " %")) +
   scale_fill_manual(values = ls_colors) +
+  scale_color_manual(values = c("grey80", "grey80", "grey80")) + 
   labs(fill = NULL, 
        x = NULL, 
-       y = "Difference in fractional savings", 
-       subtitle = "All buildings") +
-  coord_cartesian(ylim = c(-8, 8)) +
+       y = "Error in fractional savings", 
+       subtitle = str_glue("All ({nrow(df_acc_A)/3} buildings)")) +
+  coord_cartesian(ylim = c(-18, 18)) +
+  theme(panel.grid.major.y = element_line(color = "grey80", linewidth = 0.25),
+        legend.position = "none",
+        plot.margin = margin(t = 2, r = 7, b = 2, l = 2, unit = "mm"))
+
+p_top <- ggarrange(p3, p1, p2,
+          ncol = 3, nrow = 1,
+          labels = c("a)", "b)", "c)"),
+          align = "hv",
+          legend="none") +
+  plot_annotation(title = "M&V accuracy comparison", 
+                  subtitle = "with measured weather conditions")
+
+# Timeline plot
+df_time <- df_sprt_all_messy %>% 
+  filter(seq != "final") %>% 
+  mutate(set = 'messy') %>% 
+  bind_rows(df_sprt_all_tidy %>% filter(seq != "final") %>% mutate(set = 'tidy')) %>% 
+  select(name, seq, n_weeks, set)
+
+count <- list()
+n <- 1
+for (i in seq(0, 36, by = 3)){
+  
+  df_sprt <- df_time %>% 
+    filter(seq == "sprt",
+           n_weeks <= i)
+  
+  df_eob <- df_time %>% 
+    filter(seq == "eob",
+           n_weeks <= i)
+  
+  df_temp <- df_time %>% 
+    filter(seq == "temp", 
+           n_weeks <= i)
+  
+  n_tidy <- df_temp %>% 
+    filter(set == "tidy")
+  
+  n_messy <- df_temp %>% 
+    filter(set == "messy")
+  
+  count[[n]] <- tibble("n_weeks" = i, 
+                       "eob" = nrow(df_eob), 
+                       "temp" = nrow(df_temp), 
+                       "sprt" = nrow(df_sprt), 
+                       "tidy" = nrow(n_tidy), 
+                       "messy" = nrow(n_messy))
+  
+  n <- n + 1
+}
+
+count <- bind_rows(count) 
+
+p_bottom <- count %>% 
+  ggplot() +
+  geom_bar(data = . %>% 
+             filter(n_weeks == 24 | n_weeks == 36) %>% 
+             pivot_longer(c(tidy, messy), names_to = "set", values_to = "value") %>% 
+             mutate(set = as.factor(set), 
+                    set = recode_factor(set, "tidy" = "Buildings (tidy) finishing randomized M&V", "messy" = "Buildings (messy) finishing randomized M&V")), 
+           aes(x = n_weeks, y = value, fill = set), 
+           stat = "identity", 
+           position = "stack",
+           alpha = 0.8, 
+           width = 3) +
+  geom_line(aes(x = n_weeks, y = temp, color = "Buildings satisfying 80% TMY range"),
+            alpha = 0.2) +
+  geom_point(aes(x = n_weeks, y = temp, color = "Buildings satisfying 80% TMY range"), 
+             size = 1.5) +
+  geom_line(aes(x = n_weeks, y = sprt, color = "Buildings satisfying SPRT"),
+            alpha = 0.2) +
+  geom_point(aes(x = n_weeks, y = sprt, color = "Buildings satisfying SPRT"), 
+             size = 1.5, 
+             shape = 17) +
+  geom_segment(aes(x = 36.5, y = max(eob), xend = 95.5, yend = max(eob)),
+               arrow = arrow(length = unit(0.3, "in")),   
+               linewidth = 2,  
+               color = "#fb8072") + 
+  annotate(geom = "text", 
+           x = 66, 
+           y = max(count$eob) - 30, 
+           size = 5,
+           label = "Excess time by conventional M&V") +
+  geom_vline(xintercept = c(12, 24, 36, 48, 96), lty = "dashed", color = "grey80") +
+  annotate(geom = "text", 
+           x = seq(6, 48, by = 12), 
+           y = 200, 
+           label = paste0("12-week\nblock"), 
+           alpha = 0.5) +
+  scale_fill_manual(values = ls_colors) +
+  scale_color_manual(values = ls_colors) +
+  scale_x_continuous(expand = c(0, 0), 
+                     limits = c(0, 100),
+                     breaks = c(12, 24, 36, 48, 96), 
+                     labels = c("12 weeks", "24 weeks", "36 weeks", "1 year", "2 years")) +
+  coord_cartesian(ylim = c(0, 650)) +
+  labs(x = NULL, 
+       y = "Number of buildings", 
+       fill = NULL, 
+       color = NULL, 
+       title = "M&V timeline comparison") +
   theme(panel.grid.major.y = element_line(color = "grey80", linewidth = 0.25),
         legend.direction = "horizontal",
         legend.position = "bottom",
         plot.margin = margin(t = 2, r = 7, b = 2, l = 2, unit = "mm"))
 
-ggarrange(p3, p1, p2,
-          ncol = 3, nrow = 1,
-          labels = c("a)", "b)", "c)"),
-          align = "hv",
-          legend="none") +
-  plot_annotation(title = "Savings estimation accuracy comparison", 
-                  subtitle = "with measured weather conditions")
 
+ggarrange(p_top, p_bottom, 
+          ncol = 1, nrow = 2,
+          heights = c(1.5, 1),
+          labels = c(" ", "d)"),
+          legend = "bottom")
+
+ggsave(filename = "graphical_abs.png", path = fig_path, units = "in", height = 9, width = 12, dpi = 300)
